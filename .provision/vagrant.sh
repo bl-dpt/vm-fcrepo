4335259fd6f52906680dcfdbf4bc6decfb588f91
Alecs Geuder <alecs.geuder@bl.uk>
2014-09-23 08:59:08 +0100

    running docker container with fedora war
    * for now not using ansible;
    * vagrant is the wrapper for creating an ubuntu 'host' (it also
      installs docker automatically, packer is provided via an install
      script)
    * the ubuntu host creates a docker container using packer; the
      provisioning is currently done using inline shellscripts

--- .provision/vagrant.sh
+++ .provision/vagrant.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# These shell commands provision the vagrant box, which is the host
+# for the docker containers we're building.
+# It needs packer installed in case we gonna use it.
+# Alternatively we can use ansible directly (without using the ansible-local
+# provisioner from packer), in this case we have to install it.
+# Once vagrant is up and running, do packer build <path-to-packer-json>
+
+# NOTE: have implemented a basic proxy detection that can deal with
+# nmtl proxies using cntml; this is not done properly and needs the
+# vagrant-proxyconf plugin installed
+# (see https://github.com/tmatilai/vagrant-proxyconf), the docker containers
+# are simply hooked via --net=host and setting their env vars as well.
+
 PACKER_VERSION="packer_0.7.1_linux_amd64"
 PACKER_URL="https://dl.bintray.com/mitchellh/packer/$PACKER_VERSION.zip"
 PACKER_BINS=/usr/local/packer
